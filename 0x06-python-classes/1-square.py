308e1200c25cf1891f8707bd91f97735105cb994
nemlic <nemlicmain22@gmail.com>
2023-08-31 03:06:46 +0300

    task completion

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py by Nelson Itaaru
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing new square class
+        Args: size - the size of the new square defined
+        """
+
+        self.__size = size
